789cacbf7f5c8072d53400e0640798fe31a97023
VladComplus <r2d@ipodshipnik.com.ua>
2026-04-27 20:46:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,3 +26,6 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)

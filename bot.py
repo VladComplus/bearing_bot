383353efe777b2dc016f9d96b70a25749f68a746
VladComplus <r2d@ipodshipnik.com.ua>
2026-04-27 21:23:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,3 +29,8 @@ if __name__ == "__main__":
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+from aiogram.types import Message
+
+@dp.channel_post()
+async def get_channel_id(message: Message):
+    print("CHANNEL ID:", message.chat.id)
